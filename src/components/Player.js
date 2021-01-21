1c7ae2d9d95ac2ea32053420ce4c65dc732e6742
MartaKirchgessner <martakirchgessner@gmail.com>
2021-01-21 13:43:35 +0100

    pridani dat

--- src/components/Player.js
+++ src/components/Player.js
@@ -14,10 +14,15 @@ const Player = () => {
         <input type="range" />
         <p>End Time</p>
       </div>
-      <div className="play-control"></div>
-      <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-      <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
-      <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
+      <div className="play-control">
+        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon
+          className="skip-forward"
+          size="2x"
+          icon={faAngleRight}
+        />
+      </div>
     </div>
   );
 };
